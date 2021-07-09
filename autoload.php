fc11808f81fa19a3ee4ffbfe371f10d482f13793
agostinhosamil <agostinhosaml832@gmail.com>
2021-07-09 12:27:42 +0100

    root: init

---
+++ autoload.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @version 2.0
+ * @author Sammy
+ *
+ * @keywords Samils, ils, php framework
+ * -----------------
+ * @namespace Sammy\Packs\CapsuleXMLParser
+ * - Autoload, application dependencies
+ */
+namespace Sammy\Packs\CapsuleXMLParser {
+}
